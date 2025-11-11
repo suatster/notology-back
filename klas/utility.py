39b67d914345e58e2f87dc951ed9a4b4e227311a
GurhanAras <gurhanaras1665@gmail.com>
2025-11-12 02:11:10 +0300

    ....
    Bu dosyalar hala geçicidir.

---
+++ klas/utility.py
@@ -0,0 +1,14 @@
+from passlib.context import CryptContext
+
+# ---------------------------
+# Şifre hashleme ayarları
+# ---------------------------
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(plain_password: str,) -> str:
+    return pwd_context.hash(plain_password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
