650c5ca79c96dd2d9d44dd61ceffd020cf4c04fa
GurhanAras <gurhanaras1665@gmail.com>
2025-12-26 18:54:30 +0300

    ai chat

--- app/routers/chat.py
+++ app/routers/chat.py
@@ -1,35 +1,28 @@
 import httpx
-from fastapi import APIRouter, HTTPException
+import json
+from fastapi import APIRouter, HTTPException, Query
 from app.core.config import settings
+from .. import schemas 
 
 router = APIRouter(prefix="/ai", tags=["AI"])
 
-# @router.get("/chat")
-# def get_ai_response():
-#     try:
-#         response = requests.get(settings.AI_ORIGIN)
-#         response.raise_for_status()
-#         return {"message": response.text}
-#     except requests.RequestException as e:
-#         raise HTTPException(
-#             status_code=503,
-#             detail=f"AI service error: {e}"
-#         )
-    
-# import httpx
-# from fastapi import APIRouter, HTTPException
-# from app.core.config import settings
-
-# router = APIRouter(prefix="/ai", tags=["AI"])
-
 
 @router.post("/chat")
-async def get_ai_response():
+async def get_ai_response(request: schemas.ChatRequest):
     try:
-        async with httpx.AsyncClient(timeout=5.0) as client:
-            response = await client.get(settings.AI_ORIGIN)
+        async with httpx.AsyncClient(timeout=15.0) as client:
+            response = await client.post(
+                settings.AI_ORIGIN + "/api/generate",
+                json={
+                    "model": "mistral:latest" if request.model == "freud" else "wizard-math:7b",
+                    "prompt": request.prompt,
+                    "stream": False
+                }
+            )
             response.raise_for_status()
-            return {"message": response.text}
+            content = json.loads(response.content.decode("utf-8"))
+            msg = content.get("response")
+            return {"reply": msg}
     except httpx.TimeoutException:
         raise HTTPException(status_code=504, detail="AI service timeout")
     except httpx.HTTPStatusError as e:
@@ -38,5 +31,4 @@ async def get_ai_response():
             detail=f"AI service returned error: {e.response.text}"
         )
     except httpx.RequestError as e:
-        raise HTTPException(status_code=503, detail=f"AI service request failed: {str(e)}")    
-    
+        raise HTTPException(status_code=503, detail=f"AI service request failed: {str(e)}")   
